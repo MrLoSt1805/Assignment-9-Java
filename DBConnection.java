c6a9558ced68b2c4002ed837009c1c639397e83a
daniel john jacob <danieljj1805@gmail.com>
2025-04-18 23:09:03 +0530

    final commit

--- DBConnection.java
+++ DBConnection.java
@@ -3,7 +3,7 @@ import java.sql.*;
 public class DBConnection {
     private static final String URL = "jdbc:mysql://localhost:3306/studentdb";
     private static final String USER = "root";
-    private static final String PASSWORD = "";
+    private static final String PASSWORD = "dani2004";
 
     public static Connection getConnection() throws SQLException {
         try {
