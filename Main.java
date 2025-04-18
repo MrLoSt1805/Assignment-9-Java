c641d45b2cd12d86854370137866b4e25159171a
daniel john jacob <danieljj1805@gmail.com>
2025-04-18 22:43:53 +0530

    added main file

---
+++ Main.java
@@ -0,0 +1,10 @@
+// Name: Daniel John Jacob
+// PRN: 23070126027
+// Batch: AIML A2
+
+public class Main {
+    public static void main(String[] args) {
+        StudentApp app = new StudentApp();
+        app.menu();
+    }
+}
