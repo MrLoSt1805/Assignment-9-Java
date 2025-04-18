a53097850596c32eb54309b424cfba56c1023cdb
daniel john jacob <danieljj1805@gmail.com>
2025-04-18 23:01:11 +0530

    changed code for error fixing

--- DBConnection.java
+++ DBConnection.java
@@ -6,6 +6,13 @@ public class DBConnection {
     private static final String PASSWORD = "";
 
     public static Connection getConnection() throws SQLException {
+        try {
+            // Load the MySQL JDBC driver
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            throw new SQLException("MySQL JDBC Driver not found.", e);
+        }
+
         return DriverManager.getConnection(URL, USER, PASSWORD);
     }
 }
